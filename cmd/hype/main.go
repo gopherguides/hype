db37256468384fb6606236fe02c1e99ab97e8eae
Mark Bates <mark@markbates.com>
2021-09-09 16:42:51 -0400

    judge

--- cmd/hype/main.go
+++ cmd/hype/main.go
@@ -12,18 +12,13 @@ import (
 )
 
 func main() {
-	// rt, err := runtime(os.Args[1:])
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
 	if err := run(); err != nil {
 		log.Fatal(err)
 	}
 }
 
 func run() error {
-	sw := cleo.NewSwitcher()
+	sw := cleo.NewRouter()
 	sw.SetDefault(defaultSW(sw))
 
 	rt, err := cleo.NewRuntime("hype", os.Args[1:])
@@ -34,7 +29,7 @@ func run() error {
 	return sw.Switch(rt)
 }
 
-func defaultSW(sw *cleo.Switcher) cleo.SwitchFn {
+func defaultSW(sw *cleo.Router) cleo.HandlerFn {
 	return func(rt *cleo.Runtime) error {
 		var err error
 		pwd := os.Getenv("MARKED_ORIGIN")
@@ -70,164 +65,3 @@ func defaultSW(sw *cleo.Switcher) cleo.SwitchFn {
 		return nil
 	}
 }
-
-// func run(rt *Runtime) error {
-
-// 	return nil
-// 	// args := rt.Args
-
-// 	// if len(args) == 0 {
-// 	// 	return stream(rt)
-// 	// }
-// 	// switch args[0] {
-// 	// case "json":
-// 	// 	rt.Args = args[1:]
-// 	// 	return jsonCmd(rt)
-// 	// case "toc":
-// 	// 	rt.Args = args[1:]
-// 	// 	return toc(rt)
-// 	// }
-
-// 	// doc, err := parseFile(rt)
-// 	// if err != nil {
-// 	// 	rt.Usage()
-// 	// 	return err
-// 	// }
-
-// 	// return print(rt.Stdout, doc)
-// }
-
-// func print(w io.Writer, doc *hype.Document) error {
-// 	p := hype.NewPrinter(w)
-// 	// p.SetTransformer(func(tag hype.Tag) (hype.Tag, error) {
-// 	// 	inc, ok := tag.(*hype.Include)
-// 	// 	if !ok {
-// 	// 		return tag, nil
-// 	// 	}
-
-// 	// 	src := inc.Src()
-// 	// 	// base := filepath.Base(src)
-// 	// 	dir := filepath.Dir(src)
-
-// 	// 	for _, code := range inc.Children.AllType(&hype.Image{}) {
-// 	// 		sc, ok := code.(*hype.Image)
-// 	// 		if !ok {
-// 	// 			continue
-// 	// 		}
-// 	// 		x := sc.Src()
-// 	// 		if strings.HasPrefix(x, "http") {
-// 	// 			continue
-// 	// 		}
-// 	// 		x = filepath.Join(dir, x)
-// 	// 		sc.Set("src", x)
-// 	// 	}
-
-// 	// 	return inc, nil
-
-// 	// })
-
-// 	return p.Print(doc.Children...)
-// }
-
-// func toc(rt *Runtime) error {
-// 	// doc, err := parseFile(rt)
-// 	// if err != nil {
-// 	// 	rt.Usage()
-// 	// 	return err
-// 	// }
-
-// 	// err = hype.Print(doc.Children, rt.Stdout, func(w io.Writer, t hype.Tag) error {
-// 	// 	text := t.GetChildren().String()
-// 	// 	switch t.Atom() {
-// 	// 	case atom.H1:
-// 	// 		fmt.Fprintln(w, text)
-// 	// 		return nil
-// 	// 	case atom.H2:
-// 	// 		fmt.Fprintf(w, "  %s\n", text)
-// 	// 	case atom.H3:
-// 	// 		fmt.Fprintf(w, "    %s\n", text)
-// 	// 	case atom.H4:
-// 	// 		fmt.Fprintf(w, "      %s\n", text)
-// 	// 	case atom.H5:
-// 	// 		fmt.Fprintf(w, "        %s\n", text)
-// 	// 	case atom.H6:
-// 	// 		fmt.Fprintf(w, "          %s\n", text)
-// 	// 	}
-// 	// 	return nil
-// 	// })
-
-// 	// return err
-
-// 	return nil
-// }
-
-// func jsonCmd(rt *Runtime) error {
-// 	doc, err := parseFile(rt)
-// 	if err != nil {
-// 		rt.Usage()
-// 		return err
-// 	}
-
-// 	enc := json.NewEncoder(rt.Stdout)
-
-// 	if rt.IndentJSON {
-// 		enc.SetIndent("", "  ")
-// 	}
-
-// 	if err := enc.Encode(doc); err != nil {
-// 		return err
-// 	}
-
-// 	return nil
-// }
-
-// func stream(rt *Runtime) error {
-// 	pwd := os.Getenv("MARKED_ORIGIN")
-// 	if len(pwd) == 0 {
-// 		pwd, _ = os.Getwd()
-// 	}
-
-// 	cab, err := fsx.DirFS(pwd)
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	p, err := hype.NewParser(cab)
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	b, err := ioutil.ReadAll(rt.Stdin)
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	doc, err := p.ParseMD(b)
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	fmt.Fprintln(rt.Stdout, doc.String())
-// 	return nil
-// 	// return print(rt.Stdout, doc)
-// }
-
-// func parseFile(rt *Runtime) (*hype.Document, error) {
-// 	args := rt.Args
-
-// 	if len(args) == 0 {
-// 		return nil, fmt.Errorf("missing file name")
-// 	}
-
-// 	fn := args[0]
-// 	base := filepath.Base(fn)
-// 	dir := filepath.Dir(fn)
-// 	rt.Cab = os.DirFS(dir)
-
-// 	p, err := hype.NewParser(rt.Cab)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-
-// 	return p.ParseFile(base)
-// }
--- go.mod
+++ go.mod
@@ -1,13 +1,19 @@
 module github.com/gopherguides/hype
 
-go 1.16
+go 1.17
+
+// replace github.com/markbates/cleo => ../cleo
 
 require (
-	github.com/markbates/cleo v0.0.0-20210908002610-cfca2ae32c3e
+	github.com/markbates/cleo v0.0.0-20210908183044-d564be950acc
 	github.com/markbates/fsx v1.0.0
 	github.com/russross/blackfriday/v2 v2.1.0
 	github.com/stretchr/testify v1.7.0
-	golang.org/x/net v0.0.0-20210907225631-ff17edfbf26d
+	golang.org/x/net v0.0.0-20210908191846-a5e095526f91
 )
 
-// replace github.com/markbates/cleo => ../cleo
+require (
+	github.com/davecgh/go-spew v1.1.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
+)
