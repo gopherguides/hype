0a4f605dc9764e137cbbf8d872832c7654eaa937
Mark Bates <mark@markbates.com>
2021-11-03 01:08:23 -0400

    yucky atoms

--- atomx/atomx.go
+++ atomx/atomx.go
@@ -1,13 +1,1 @@
 package atomx
-
-import (
-	adam "github.com/gopherguides/hype/atomx/internal/atom"
-	"golang.org/x/net/html/atom"
-)
-
-const (
-	File      = atom.Atom(adam.File)
-	FileGroup = atom.Atom(adam.Filegroup)
-	Include   = atom.Atom(adam.Include)
-	Page      = atom.Atom(adam.Page)
-)
